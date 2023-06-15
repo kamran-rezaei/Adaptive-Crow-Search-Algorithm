75b232520f724883dfdc2c3bd5c529be82d1f824
Kamran Rezaei <36191813+kamran5775@users.noreply.github.com>
2023-06-15 18:51:07 +0330

    Update ObjectiveFunction.m

--- ObjectiveFunction.m
+++ ObjectiveFunction.m
@@ -1,3 +1,17 @@
+%_______________________________________________________________________________________%
+%  Adaptive crow search algorithm (ACSA) source codes                                   %
+%                                                                                       %
+%  Developed in MATLAB R2016b                                                           %
+%  Author and programmer: Kamran Rezaei                                                 %
+%  Rezaei, K, Rezaei, H,                                                                %
+%  e-Mail: kamran.rezaei6990@gmail.com                                                  %
+%                                                                                       %
+% Main paper:                                                                           %
+% Rezaei, K., Rezaei, H., Adaptive crow search algorithm and its application in solving 
+% constrained optimization problems,   %
+%Aquila Optimizer: A novel meta-heuristic optimization Algorithm, 3rd International 
+% Conference on Soft Computing (2019), 2260-2269
+%_______________________________________________________________________________________%
 
 function o = ObjectiveFunction(x)
 
